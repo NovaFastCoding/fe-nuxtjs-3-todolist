9299429fd2ea6e698ffda3203228f2743ed38145
nova <nova.fastcodingvn@gmail.com>
2023-07-06 13:31:46 +0700

    Initialize project

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="wrapper">
+		<div class="content">
+			<AddTodo />
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AddTodo from './src/components/addTodo/index.vue';
+
+export default defineComponent({
+	name: 'App',
+	components: {
+		AddTodo,
+	},
+});
+</script>
+
+<style>
+@import './src/assets/global.scss';
+@import './src/assets/app.scss';
+</style>
---
+++ src/components/addTodo/index.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="addTodo">
+		<div class="nameTodo">
+			<input type="text" v-model="newTodo.name" placeholder="Your todo..." />
+		</div>
+		<input type="date" v-model="newTodo.deadline" />
+		<button @click="addNewTodo">+</button>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'addTodo',
+	data() {
+		return {
+			newTodo: {
+				name: '',
+				deadline: '',
+			},
+		};
+	},
+	methods: {
+		addNewTodo() {
+			if (this.newTodo.name && this.newTodo.deadline) {
+				const newTodo = {
+					id: Date.now(),
+					name: this.newTodo.name,
+					deadline: this.newTodo.deadline,
+					isCompleted: false,
+				};
+
+				console.log(newTodo);
+
+				this.newTodo = {
+					name: '',
+					deadline: '',
+				};
+			}
+		},
+	},
+});
+</script>
+
+<style scoped>
+@import './index.scss';
+</style>
